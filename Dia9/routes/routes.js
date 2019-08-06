6d7b82ee0833152004914bc90fdebe62e3a08a73
gerardomdn95 <gerardo@devf.mx>
2019-08-05 21:00:50 -0500

    MONGO funciona

--- Dia9/routes/routes.js
+++ Dia9/routes/routes.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const router = express.Router();
 
+const { Movie } = require("../models/movie");
+
 router.get("/", (req, res) => {
     res.status(200).send({response: "Corriendo desde routes"});
+});
+
+router.post("/api/v1/pelicula", (req, res) => {
+    const { title, year, description, image, theme, director } = req.body;
+    const newMovie = Movie({
+        title, 
+        year, 
+        description, 
+        image, 
+        theme, 
+        director
+    });
+    newMovie.save((err, pelicula) => {
+        err ? res.status(409).send(err.message) : res.status(201).send(pelicula)
+    });
+});
+
+router.get("/api/v1/pelicula", (req, res) => {
+    Movie.find().exec()
+        .then(movies => res.status(200).send(movies))
+        .catch(err => res.status(404).send(err));
+});
+
+router.get("/api/v1/peliculaById/", (req, res) => {
+    const { id } = req.query;
+    Movie.findById(id).exec()
+        .then(movies => res.status(200).send(movies))
+        .catch(err => res.status(404).send(err));
 });
 
 module.exports = { router };
