f67c9c4b98cd9c706e045c71971b109ad56e37d1
gerardomdn95 <gerardo@devf.mx>
2019-08-06 20:09:13 -0500

    CRUD MONGODB

--- Dia9/routes/routes.js
+++ Dia9/routes/routes.js
@@ -4,17 +4,18 @@ const router = express.Router();
 const { Movie } = require("../models/movie");
 
 router.get("/", (req, res) => {
-    res.status(200).send({response: "Corriendo desde routes"});
+    res.status(200).send({ response: "Corriendo desde routes" });
 });
 
+// CREATE
 router.post("/api/v1/pelicula", (req, res) => {
     const { title, year, description, image, theme, director } = req.body;
     const newMovie = Movie({
-        title, 
-        year, 
-        description, 
-        image, 
-        theme, 
+        title,
+        year,
+        description,
+        image,
+        theme,
         director
     });
     newMovie.save((err, pelicula) => {
@@ -22,6 +23,7 @@ router.post("/api/v1/pelicula", (req, res) => {
     });
 });
 
+// READ
 router.get("/api/v1/pelicula", (req, res) => {
     Movie.find().exec()
         .then(movies => res.status(200).send(movies))
@@ -33,6 +35,64 @@ router.get("/api/v1/peliculaById/", (req, res) => {
     Movie.findById(id).exec()
         .then(movies => res.status(200).send(movies))
         .catch(err => res.status(404).send(err));
+});
+
+router.get("/api/v1/busqueda/pelicula", (req, res) => {
+    const { q } = req.query;
+    Movie.find({ title: q }).exec()
+        .then(movie => {
+            movie.length > 0 
+                ? res.status(200).send(movie)
+                : res.status(404).send("Not found")
+        })
+        .catch(err => res.status(404).send(err))
+});
+
+// UPDATE
+
+// Modificar todo el objeto o registro
+router.put("/api/v1/peliculas/update", (req, res) => {
+    const { id } = req.query;
+    const body = req.body;
+
+    Movie.findByIdAndUpdate(id, { $set: body }, { new: true })
+        .then(newMovie => {
+            if(newMovie !== null) {
+                res.status(202).send(newMovie)
+            } else {
+                res.status(304).send("Registro no encontrado, imposible modificar")
+            }
+        })
+        .catch(err => res.status(404).send(err))
+});
+
+// Modificar parcialmente el registro
+router.patch("/api/v1/peliculas/update", (req, res) => {
+    const { id } = req.query;
+    const body = req.body;
+
+    Movie.findByIdAndUpdate(id, { $set: body }, { new: true })
+        .then(newMovie => {
+            if(newMovie !== null) {
+                res.status(202).send(newMovie)
+            } else {
+                res.status(304).send("Registro no encontrado, imposible modificar")
+            }
+        })
+        .catch(err => res.status(404).send(err))
+});
+
+// DELETE
+router.delete("/api/v1/peliculas/delete", (req, res) => {
+    const { id } = req.query;
+
+    Movie.findByIdAndRemove(id).exec()
+        .then(pelicula => {
+            pelicula !== null 
+            ? res.status(200).send({ mensaje: "Película borrada exitosamente", body: pelicula })
+            : res.status(304).send({ mensaje: "Registro no eliminado " })
+        })
+        .catch(err => res.status(304).send({ mensaje: "Registro no eliminado " }))
 });
 
 module.exports = { router };
