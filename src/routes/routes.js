6a09bd9e1d2f96c1ca4267b105d1eccab94d189b
gerardomdn95 <gerardo@devf.mx>
2019-08-09 11:07:28 -0500

    CORS

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,40 @@ const path = require('path');
 const cors = require("cors");
 const router = express.Router();
 
-router.use(cors());
+// hard coded configuration object
+conf = {
+    // look for PORT environment variable,
+    // else look for CLI argument,
+    // else use hard coded value for port 8080
+    port: process.env.PORT || process.argv[2] || 3000,
+    // origin undefined handler
+    // see https://github.com/expressjs/cors/issues/71
+    originUndefined: function (req, res, next) {
+        if (!req.headers.origin) {
+            res.json({
+                mess: 'Hi you are visiting the service locally. If this was a CORS the origin header should not be undefined'
+            });
+        } else {
+            next();
+        }
+    },
+    // Cross Origin Resource Sharing Options
+    cors: {
+        // origin handler
+        origin: function (origin, cb) {
+            // setup a white list
+            let wl = ['https://localhost:3000'];
+            if (wl.indexOf(origin) != -1) {
+                cb(null, true);
+            } else {
+                cb(new Error('invalid origin: ' + origin), false);
+            }
+        },
+        optionsSuccessStatus: 200
+    }
+};
+// use origin undefined handler, then cors for all paths
+app.use(conf.originUndefined, cors(conf.cors));
 
 const { Movie } = require("../models/movie");
 
